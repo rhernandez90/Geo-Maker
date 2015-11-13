d9023b11d8085ae7ca7ed9c0e5d8b2fb8986e740
rhernandez90 <rhernandezmunguia@gmail.com>
2015-11-12 22:32:48 -0600

    Inicio Geo Maker
    Inicio Geo Maker

---
+++ Angular/app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularApp
+ * @description
+ * # angularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularApp', [
+    'ngAnimate',
+    'ngRoute',
+    'leaflet-directive'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/mapa',{
+        templateUrl: 'views/mapa.html',
+        controller: 'MapaCtrl',
+        controllerAs: 'Mapa'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ Angular/app/scripts/controllers/mapa.js
@@ -0,0 +1,70 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularApp
+ */
+angular.module('angularApp')
+  .controller('MapaCtrl', function ($scope) {
+
+
+            angular.extend($scope, {
+          center: {
+              lat: 15.529,
+              lng: -88.027,
+              zoom: 15
+          },
+          layers: {
+                baselayers: {
+                    googleTerrain: {
+                        name: 'Google Terrain',
+                        layerType: 'TERRAIN',
+                        type: 'google'
+                    },
+                    googleHybrid: {
+                      name: 'Google Hybrid',
+                      layerType: 'HYBRID',
+                      type: 'google'
+                    },
+                    bingAerial: {
+                            name: 'Bing Aerial',
+                            type: 'bing',
+                            key: 'Aj6XtE1Q1rIvehmjn2Rh1LR2qvMGZ-8vPS9Hn3jCeUiToM77JFnf-kFRzyMELDol',
+                            layerOptions: {
+                                type: 'Aerial'
+                            }
+                        },
+                    googleRoadmap: {
+                        name: 'Google Streets',
+                        layerType: 'ROADMAP',
+                        type: 'google'
+                   },
+                   osm: {
+                         name: 'OpenStreetMap',
+                         type: 'xyz',
+                         url: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                         layerOptions: {
+                         subdomains: ['a', 'b', 'c'],
+                         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                         continuousWorld: true
+                     }
+                   }
+
+                 }
+
+            },
+          overlays: {
+                        draw: {
+                            name: 'draw',
+                            type: 'group',
+                            visible: true,
+                            layerParams: {
+                                showOnSelector: false
+                            }
+                        }
+                    }
+        });
+  });
